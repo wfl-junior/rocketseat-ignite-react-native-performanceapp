420d20c4e94eb779bc427b4d362f6274a517339e
Wallace Júnior <wflj1997@hotmail.com>
2022-07-15 15:02:21 -0300

    useMemo

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -1,8 +1,14 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { useMemo } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { Friend } from "../screens/Home";
 import { FriendItem } from "./FriendItem";
 
 const styles = StyleSheet.create({
+  totalLikes: {
+    marginTop: 20,
+    fontSize: 18,
+    fontWeight: "700",
+  },
   list: {
     marginTop: 20,
   },
@@ -12,10 +18,20 @@ interface FriendListProps {
   friends: Friend[];
 }
 
-export const FriendList: React.FC<FriendListProps> = ({ friends }) => (
-  <ScrollView showsVerticalScrollIndicator={false} style={styles.list}>
-    {friends.map(friend => (
-      <FriendItem key={friend.id} data={friend} />
-    ))}
-  </ScrollView>
-);
+export const FriendList: React.FC<FriendListProps> = ({ friends }) => {
+  const totalLikes = useMemo(() => {
+    return friends.reduce((likes, friend) => likes + friend.likes, 0);
+  }, [friends]);
+
+  return (
+    <View>
+      <Text style={styles.totalLikes}>Total de likes: {totalLikes}</Text>
+
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.list}>
+        {friends.map(friend => (
+          <FriendItem key={friend.id} data={friend} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
